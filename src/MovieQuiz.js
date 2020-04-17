c89923622732f43d9b827f5096cb979f0d5b98b3
Tomasz-Szkonter <44675487+Tomasz-Szkonter@users.noreply.github.com>
2020-04-18 00:36:04 +0200

    v0.1
    1st static view of main page.
    Using bootstrap4

--- src/MovieQuiz.js
+++ src/MovieQuiz.js
@@ -1,10 +1,69 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "./bootstrap.min.css";
 
-function MovieQuiz() {
-  return <div>Movie Quiz</div>;
-}
+const Hero = () => {
+  return (
+    <div className="jumbotron col-10 offset-1 ">
+      <h1>Movie Directors Quiz</h1>
+      <p>Select the movie title directed by shown person</p>
+    </div>
+  );
+};
+
+const Movie = ({ title }) => {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  );
+};
+
+const Board = ({ director, movie }) => {
+  return (
+    <div className="row board" style={{ backgroundColor: "lightgray" }}>
+      <div className="col-4 offset-1">
+        <img
+          src={director.imageUrl}
+          className="director-image"
+          alt="Director"
+        ></img>
+      </div>
+      <div className="col-6">
+        {console.log(movie)}
+        {movie.map((title) => (
+          <Movie title={title} key={title} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Operations = () => {
+  return <></>;
+};
+
+const Footer = () => {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from <a href="https://www.imdb.com/">LINK HERE</a>.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const MovieQuiz = ({ boardData }) => {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Board {...boardData} />
+      <Operations />
+      <Footer />
+    </div>
+  );
+};
 
 export default MovieQuiz;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ import "./index.css";
 import MovieQuiz from "./MovieQuiz";
 import * as serviceWorker from "./serviceWorker";
 
+const directors = [
+  {
+    name: "Stanley Kubrick",
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BMTIwMzAwMzg1MV5BMl5BanBnXkFtZTYwMjc4ODQ2._V1_.jpg",
+    imageSource: "IMDB",
+    movies: ["A Clockwork Orange"],
+  },
+];
+
+const state = {
+  boardData: {
+    director: directors[0],
+    movie: directors[0].movies,
+  },
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <MovieQuiz />
+    <MovieQuiz {...state} />
   </React.StrictMode>,
   document.getElementById("root")
 );
